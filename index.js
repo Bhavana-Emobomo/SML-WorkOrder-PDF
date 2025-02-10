6d2a234c9938cf345b88e3410cc1f89a1869db2c
Nihar Ranjan Panda <nihar.panda@emobomo.com>
2025-02-10 10:56:51 +0530

    new fix

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ exports.handler = async (event) => {
     const tableXPositions = [25, 60, 280, 430, 480];
     const maxWidthForColumns = [30, 80, 140, 180, 100];
     
-    const rowHeight = 25;
+    const rowHeight = 60;
     const cellPadding = 5;
     const minRowHeight = 18; // Slightly increased for better text alignment
     const lineHeight = 10;
