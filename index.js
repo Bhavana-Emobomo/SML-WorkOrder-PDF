eba9978bf28fcc9f5575f2823f065828235b0a70
Nihar Ranjan Panda <nihar.panda@emobomo.com>
2025-02-10 12:38:34 +0530

    new

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ exports.handler = async (event) => {
       const rowHeight = 25;
       const cellPadding = 2;
       const minRowHeight = 18; // Slightly increased for better text alignment
-      const lineHeight = 10;
+      const lineHeight = 30;
       
       // Move horizontal lines and text slightly up
       const moveUpAmount = 3; // Fine-tuned for best alignment
@@ -198,7 +198,7 @@ exports.handler = async (event) => {
       tableHeaders.forEach((header, index) => {
         currentPage.drawText(header, {
           x: tableXPositions[index] + cellPadding,
-          y: itemY - rowHeight / 2 + 3, // Centering header text
+          y: itemY - rowHeight / 2 + headerMoveOffset, // Move header up/down
           size: 9,
           font: timesRomanFontBold,
           color: blackColor,
