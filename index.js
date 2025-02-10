f7f89f3da76999ee62f6fd78149694934faba3cb
Nihar Ranjan Panda <nihar.panda@emobomo.com>
2025-02-10 14:44:45 +0530

    fixes

--- index.js
+++ index.js
@@ -306,7 +306,7 @@ exports.handler = async (event) => {
         const rowBottomY = rowTopY - dynamicRowHeight;
 
         // Adjusted text position for centering within the row
-        const textStartY = rowTopY - dynamicRowHeight / 2 + lineHeight / 2 - 10;
+        const textStartY = rowTopY - dynamicRowHeight / 2 + lineHeight / 2 - 8;
 
         // Draw text for each column (Perfectly centered)
         currentPage.drawText(item.SNO, {
