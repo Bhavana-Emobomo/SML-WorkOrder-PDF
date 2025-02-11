08af326c30a044ddeda9e259de1a6fdd7dd71718
Nihar Ranjan Panda <nihar.panda@emobomo.com>
2025-02-11 10:32:00 +0530

    new fix

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ exports.handler = async (event) => {
         "WorkOrderId",
       ];
       const tableXPositions = [25, 60, 280, 430, 480];
-      const maxWidthForColumns = [30, 80, 140, 180, 100];
+      const maxWidthForColumns = [30, 80, 160, 180, 80];
 
       const rowHeight = 25;
       const cellPadding = 2;
